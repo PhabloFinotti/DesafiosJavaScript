8e3c14fb62afadfd392de7c7f610c748de206d22
Phablo Finotti <phablofinotti@gmail.com>
2020-08-17 23:12:46 -0300

    Desafio 1
    Primeiro desafio do GitHub do Curso em Video.

---
+++ Desafio1/script.js
@@ -0,0 +1,13 @@
+let btnPrimeiro = document.querySelector('#primeiro')
+let btnSegundo = document.querySelector('#segundo')
+let btnTerceiro = document.querySelector('#terceiro')
+
+btnPrimeiro.addEventListener("click", () =>{
+    alert("Você clicou no 1 botão")
+})
+btnSegundo.addEventListener("click", () =>{
+    alert("Você clicou no 2 botão")
+})
+btnTerceiro.addEventListener("click", () =>{
+    alert("Você clicou no 3 botão")
+})
